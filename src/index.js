d8c1112915dbe2a94a43598a8c302e5eba91a5c0
Rolo <rolandofender@gmail.com>
2024-01-16 21:02:29 -0600

    Profile_card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <h1>My Profile</h1>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div className="profile-photo">
+      <img src={require("./profile-photo.jpg")} alt="profile" />
+    </div>
+  );
+}
+function Body() {
+  return (
+    <div>
+      <p>Profile description</p>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <div>
+      <scan>developer🤑</scan>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
